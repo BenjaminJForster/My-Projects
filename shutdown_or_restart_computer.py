59b29631b8bb1cbd3be81fd951a20e9e3c3f94fd
BenjaminJForster <68151270+BenjaminJForster@users.noreply.github.com>
2020-09-19 18:26:29 +0100

    Create shutdown_or_restart_computer.py

---
+++ shutdown_or_restart_computer.py
@@ -0,0 +1,13 @@
+import os
+
+print('1. Shutdown Computer')
+print('2. Restart Computer')
+print('3. Exit Program')
+choice = int(input('\nEnter your choice: '))
+if(choice>=1 and choice<=2):
+    if choice == 1:
+        os.system('shutdown /s /t 1');
+    else:
+        os.system('shutdown /r /t 1');
+else:
+    exit()
